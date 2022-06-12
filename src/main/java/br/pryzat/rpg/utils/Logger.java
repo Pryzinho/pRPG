14d0319ef87971c6ffa1a5d332011f015184ed4f
Pryzinho <jheffersonsouza.pry@gmail.com>
2022-06-12 15:02:01 -0300

    Melhorando a mensagem de inicialização.

--- src/main/java/br/pryzat/rpg/utils/Logger.java
+++ src/main/java/br/pryzat/rpg/utils/Logger.java
@@ -8,12 +8,12 @@ public class Logger {
         sender.sendMessage(PryColor.color(text));
     }
     public static void logInfo(ConsoleCommandSender sender, String text){
-        sender.sendMessage(PryColor.color("&f[&b&nINFO&f] &b" + text));
+        sender.sendMessage(PryColor.color("&f[&bINFO&f] &b" + text));
     }
     public static void logWarn(ConsoleCommandSender sender, String text){
-        sender.sendMessage(PryColor.color("&f[&e&nWARNING&f] &e" + text));
+        sender.sendMessage(PryColor.color("&f[&eWARNING&f] &e" + text));
     }
     public static void logError(ConsoleCommandSender sender, String text){
-        sender.sendMessage(PryColor.color("&c[&c&nERROR&f] &c" + text));
+        sender.sendMessage(PryColor.color("&c[&cERROR&f] &c" + text));
     }
 }
